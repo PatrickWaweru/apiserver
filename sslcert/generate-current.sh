3d1978d4675dd16e9e7baeed3d210b7b722271ce
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2022-04-26 11:44:09 +0300

    fixed generate-current.sh to remove temp files

--- sslcert/generate-current.sh
+++ sslcert/generate-current.sh
@@ -3,6 +3,7 @@
 curl -s -o server.pem http://local-ip.co/cert/server.pem
 curl -s -o chain.pem http://local-ip.co/cert/chain.pem
 cat server.pem chain.pem > ./default.crt
+rm -rf server.pem chain.pem
 curl -s -o ./default.key http://local-ip.co/cert/server.key
 
 echo "Done"
